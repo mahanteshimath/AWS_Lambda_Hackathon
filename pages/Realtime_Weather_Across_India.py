7cd8a9c99986baf2dbca23a21d4e9d87a536ca44
Mahantesh Hiremath <mahanteshimath@gmail.com>
2025-06-30 00:29:22 +0530

    ok

--- pages/Realtime_Weather_Across_India.py
+++ pages/Realtime_Weather_Across_India.py
@@ -9,11 +9,11 @@ import boto3
 ist_timezone = pytz.timezone('Asia/Kolkata')
 current_time_ist = datetime.now(ist_timezone)
 current_time_ist = current_time_ist.strftime("%Y-%m-%d %H:%M:%S")
-col1,col2 = st.columns(2)
-with col1:
-    st.image("./src/DH1.PNG", caption="What we like", use_column_width=True)
-with col2:
-    st.image("./src/DH2.PNG", caption="This is Now", use_column_width=True)
+# col1,col2 = st.columns(2)
+# with col1:
+#     st.image("./src/DH1.PNG", caption="What we like", use_column_width=True)
+# with col2:
+#     st.image("./src/DH2.PNG", caption="This is Now", use_column_width=True)
 
 def execute_query(query):
     try:
